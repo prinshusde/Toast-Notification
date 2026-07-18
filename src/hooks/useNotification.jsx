472ee18447925e095975c9039abdd3e2674cd15f
Prince Shukla <prinshusdework@gmail.com>
2026-07-18 22:07:26 +0530

    can close toast

--- src/hooks/useNotification.jsx
+++ src/hooks/useNotification.jsx
@@ -6,6 +6,14 @@ import { v4 as uuidv4 } from 'uuid';
 const useNotification = (position="top-right")=>{
     const [notifications,setNotifications] = useState([])
 
+    const handleCloseNotification = (index)=>{
+        setNotifications((preNotifications)=>{
+            const updatedNotifications = [...preNotifications]
+                updatedNotifications.splice(index,1)
+                return updatedNotifications
+        })
+    }
+
     const triggerNotification = useCallback((notificationProps)=>{
         const toastId = uuidv4();
         setNotifications((preNotification)=>[
@@ -24,8 +32,8 @@ const useNotification = (position="top-right")=>{
     const NotificationComponent = notifications?(
          <div className={`notification-container ${position} ${position.split("-")[0]}`}>
             {
-                notifications.map((notification)=>{
-                    return <Notification key={notification.id} {...notification} onClose={()=>{}}/>
+                notifications.map((notification,index)=>{
+                    return <Notification key={notification.id} {...notification} onClose={()=>handleCloseNotification(index)}/>
                 })
             }
             
